800b43d5a8aa7220b404ef52071377ffff4cc85e
sarupahari <sarupahari@gmail.com>
2018-02-11 12:28:22 -0600

    Initial commit

---
+++ app/src/main/java/edu/wtamu/cis/cidm4385saru/criminalintent/Crime.java
@@ -0,0 +1,51 @@
+package edu.wtamu.cis.cidm4385saru.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+
+/**
+ * Created by sarup on 2/8/2018.
+ */
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+}
