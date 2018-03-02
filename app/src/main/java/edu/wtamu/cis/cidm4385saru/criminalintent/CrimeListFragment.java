ac196077470e29bf343f04e3266fff8e3bce5f55
sarupahari <sarupahari@gmail.com>
2018-03-01 20:05:10 -0600

    Chapter10

--- app/src/main/java/edu/wtamu/cis/cidm4385saru/criminalintent/CrimeListFragment.java
+++ app/src/main/java/edu/wtamu/cis/cidm4385saru/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package edu.wtamu.cis.cidm4385saru.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -36,9 +37,22 @@ public class CrimeListFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onResume(){
+        super.onResume();
+        updateUI();
+    }
+
     private void updateUI(){
         CrimeLab crimelab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimelab.getCrimes();
+
+        if (mAdapter == null){
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        } else{
+            mAdapter.notifyDataSetChanged();
+        }
 
         mAdapter = new CrimeAdapter(crimes);
         mCrimeRecyclerView.setAdapter(mAdapter);
@@ -71,9 +85,8 @@ public class CrimeListFragment extends Fragment {
         }
         @Override
         public void onClick(View view) {
-            Toast.makeText(getActivity(),
-                    mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT)
-                    .show();
+            Intent intent = CrimeActivity. newIntent(getActivity(), mCrime.getId());
+            startActivity(intent);
         }
     }
 
