2b99301daa4a087cd8688b6285b21076323ebce1
sarupahari <sarupahari@gmail.com>
2018-03-25 02:41:22 -0500

    Chapter14

--- app/src/main/java/edu/wtamu/cis/cidm4385saru/criminalintent/Crime.java
+++ app/src/main/java/edu/wtamu/cis/cidm4385saru/criminalintent/Crime.java
@@ -16,10 +16,13 @@ public class Crime {
     private boolean mSolved;
 
     public Crime(){
-        mId = UUID.randomUUID();
-        mDate = new Date();
+       this(UUID.randomUUID());
     }
 
+    public Crime(UUID id){
+        mId = id;
+        mDate = new Date();
+    }
     public UUID getId() {
         return mId;
     }
